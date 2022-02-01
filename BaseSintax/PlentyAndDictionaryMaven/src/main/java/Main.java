5e0fbdca3a3c0878bbab582536e929e990fd3932
Vadim Pechenin <vadim.pechenin2015@gmail.com>
2022-02-01 15:54:38 +0400

    First maven project with jar

---
+++ BaseSintax/PlentyAndDictionaryMaven/src/main/java/Main.java
@@ -0,0 +1,15 @@
+/*
+Множество и словарь
+java -jar testPro.jar - запуск собранного jar файла в терминале
+*/
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int n =in.nextInt();
+        while (n-->0)
+            System.out.println("Hello world!");
+    }
+}
